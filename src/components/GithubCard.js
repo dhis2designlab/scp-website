fd7487c9ffd8ca97138f2c156c4e6c9d2b4f23f1
goudbes <51861776+goudbes@users.noreply.github.com>
2020-08-18 14:35:21 +0200

    converted github card to hooks

--- src/components/GithubCard.js
+++ src/components/GithubCard.js
@@ -1,49 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactTooltip from 'react-tooltip';
 
-class GithubCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false,
-            item: null,
-            username: this.props.username,
-            error: null,
-            size: props.avatarSize,
-        }
-    }
+const GithubCard = (props) => {
+    const [state, setState] = useState({
+        isLoaded: false,
+        item: null,
+        username: props.username,
+        error: null,
+        size: props.avatarSize,
+    });
 
-    async componentDidMount() {
-        let result;
-        try {
-            const response = await fetch(`https://api.github.com/users/${this.state.username}`);
-            result = await response.json();
-            this.setState({
-                isLoaded: true,
-                item: result,
-            });
-        } catch (error) {
-            this.setState({
-                isLoaded: true,
-                error
-            });
-        }
-    }
+    useEffect(() => {
+        (async () => {
+            let result;
+            try {
+                const response = await fetch(`https://api.github.com/users/${state.username}`);
+                result = await response.json();
+                setState((oldState) => ({
+                    ...oldState,
+                    isLoaded: true,
+                    item: result,
+                }));
+            } catch (error) {
+                setState((oldState) => ({
+                    ...oldState,
+                    isLoaded: true,
+                    error
+                }));
+            }
+        })();
+    }, [state.username]);
 
-    render() {
-        const { error, isLoaded, item, size } = this.state;
+    const { error, isLoaded, item, size } = state;
 
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            return (
-                <>
-                    <a href={item.html_url} target="_blank" rel="noopener noreferrer"><img data-tip={item.login} alt="github avatar" style={size} src={item.avatar_url} /></a><ReactTooltip />
-                </>
-            )
-        }
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <>
+                <a href={item.html_url} target="_blank" rel="noopener noreferrer"><img data-tip={item.login} alt="github avatar" style={size} src={item.avatar_url} /></a><ReactTooltip />
+            </>
+        )
     }
 }
 
