f89fcd696a7934c16aecfa0e015c16aeb0bbfaaa
Simeon Andersen Tverdal <simeonat@student.matnat.uio.no>
2020-08-25 11:46:50 +0200

    dhis2 namespace restriction v0.1

--- src/actions/npms.js
+++ src/actions/npms.js
@@ -1,13 +1,20 @@
 import axios from 'axios'
 import { packages } from './actionTypes'
 import { npms } from '../app/config'
+import qs from 'qs'
+import { faJoint } from '@fortawesome/free-solid-svg-icons'
 
 /**
  * Simple method for fetching a set of packages from npms and then dispatching them to get stored in the Redux store.
  */
 export const getPackages = (inputValue) => async (dispatch, getState) => {
-    const response = await axios.get(npms.baseUrl, { params: { q: inputValue}})
-    console.log(response.data)
-
+    const response = await axios.get(npms.baseUrl, { 
+        params: { 
+            q: inputValue,
+        },
+        paramsSerializer: params => {
+            return qs.stringify(params, {encode: false})
+        }
+    })
     dispatch({ type: packages.fetchPackages, payload: response.data})
 }
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -7,6 +7,7 @@ const searchFieldStyle = {
         borderBottomRightRadius: 0,
         borderWidth: '0px',
         margin: 0,
+        backgroundColor: 'white',
     },
     button: {
         borderWidth: '0px',
@@ -25,6 +26,7 @@ const searchFieldStyle = {
 const SearchField = (props) => {
     const style = props.style || searchFieldStyle;
     const [inputValue, setInputValue] = useState('');
+    const [modifiers, setModifiers] = useState([]);
     /*
 
     const dispatch = useDispatch();
@@ -37,18 +39,36 @@ const SearchField = (props) => {
 
     const onClick = (e) => {
         e.preventDefault()
-        props.onSearch(inputValue);
+        var query = queryBuilder();
+        props.onSearch(query);
     }
 
     const handleChange = (e) => {
         setInputValue(e.target.value);
     }
 
+    const toggleDhis2 = (e) => {
+        e.preventDefault()
+        setModifiers([...modifiers, 'scope:dhis2'])
+      }
+
+    // To be expanded upon?
+      const queryBuilder = () => {
+        var input = inputValue
+        var appendix = modifiers.join('+');
+        appendix = '+' + appendix
+        input += appendix;
+        return input;
+      }
+
     return (
-        <form className="pure-form" style={searchFieldStyle.form}>
-            <input id="search" type="text" style={style.input} className="pure-u-1" placeholder="Search for packages here..." value={inputValue} onChange={handleChange} />
-            <button type="submit" style={style.button} className="pure-button pure-button-primary" onClick={onClick}>{props.searchButtonText !== undefined ? props.searchButtonText : `Search`}</button>
-        </form>
+        <>
+            <form className="pure-form" style={searchFieldStyle.form}>
+                <input id="search" type="text" style={style.input} className="pure-u-1" placeholder="Search for packages here..." value={inputValue} onChange={handleChange} />
+                <button type="submit" style={style.button} className="pure-button pure-button-primary" onClick={onClick}>{props.searchButtonText !== undefined ? props.searchButtonText : `Search`}</button>
+            </form>
+            <button type="button" onClick={toggleDhis2}>dhis2</button>
+        </>
     )
 }
 
