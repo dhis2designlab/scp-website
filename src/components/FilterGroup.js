adfef365bc4f5bfac730ce1ebbffb93a27809940
goudbes <51861776+goudbes@users.noreply.github.com>
2020-11-11 17:45:38 +0100

    filter group responsive

--- src/components/FilterGroup.js
+++ src/components/FilterGroup.js
@@ -5,6 +5,7 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import FormCheck from 'react-bootstrap/FormCheck'
 import { setFilterAndSearch } from '../actions/filters'
 import VersionFilter from './VersionFilter'
+import '../stylesheets/filter.css'
 
 const FilterGroup = () => {
     const dispatch = useDispatch()
@@ -12,23 +13,42 @@ const FilterGroup = () => {
     const { framework, onlyVerified, dhis2Versions } = useSelector(state => state.filter)
     const { allDhis2Versions, dhis2VersionsById } = useSelector(state => state.packages)
 
-    const handleFramework = ( newFramework ) => {
-        dispatch(setFilterAndSearch({framework: newFramework}))
+    const handleFramework = (newFramework) => {
+        dispatch(setFilterAndSearch({ framework: newFramework }))
     }
 
-    const handleVerified = ( event ) => {
-        dispatch(setFilterAndSearch({onlyVerified: event.target.checked}))
+    const handleVerified = (event) => {
+        dispatch(setFilterAndSearch({ onlyVerified: event.target.checked }))
     }
 
-    const handleDhis2Version = ( version ) => {
+    const handleDhis2Version = (version) => {
         const newVersions = [...dhis2Versions]
-        const indexOf = dhis2Versions.indexOf(version) 
-        if(indexOf === -1) newVersions.push(version)
+        const indexOf = dhis2Versions.indexOf(version)
+        if (indexOf === -1) newVersions.push(version)
         else newVersions.splice(indexOf, 1)
-        dispatch(setFilterAndSearch({dhis2Versions: newVersions}))
+        dispatch(setFilterAndSearch({ dhis2Versions: newVersions }))
     }
 
     return (
+        <div className="pure-g">
+            <div className="pure-u-1-1 pure-u-md-4-5"><div className="drop-down"><DropdownButton id="dropdown-basic-button" title={`Framework: ${framework}`} className="mr-1 drop-down">
+                <Dropdown.Item onClick={() => handleFramework('all')}>All</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleFramework('react')}>React</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleFramework('angular')}>Angular</Dropdown.Item>
+            </DropdownButton></div><VersionFilter className="filter-item" allDhis2Versions={allDhis2Versions} dhis2Versions={dhis2Versions} handleDhis2Version={handleDhis2Version} dhis2VersionsById={dhis2VersionsById} /></div>
+            <div className=" pure-u-1 pure-u-md-1-5 form-check"><FormCheck
+                id="verified-check"
+                label="Show only verified components"
+                inline={true}
+                className="ml-auto"
+                type="checkbox"
+                checked={onlyVerified}
+                onChange={handleVerified}
+            /></div>
+        </div>
+
+        /**
+         * return (
         <div className="d-flex flex-row">
             <DropdownButton id="dropdown-basic-button" title={`Framework: ${framework}`} className="mr-1">
                 <Dropdown.Item onClick={() => handleFramework('all')}>All</Dropdown.Item>
@@ -46,6 +66,8 @@ const FilterGroup = () => {
                 onChange={handleVerified}
             />
         </div>
+    )
+         */
     )
 }
 
